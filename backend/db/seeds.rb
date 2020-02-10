99a1e8a110588c556d975cd3d4dcb21da1c3cf3e
Annie Liao <liaowow@gmail.com>
2020-02-10 15:38:53 -0500

    seed data done

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -11,7 +11,12 @@ DatabaseCleaner.clean_with(:truncation)
 
 
 ## USERS
-u1 = User.create(username:"liaowow", location:"NYC", first_name:"annie", profile_pic:"")
+u1 = User.create(username:"liaowow", location:"NYC", first_name:"Annie", profile_pic:"https://ca.slack-edge.com/T02MD9XTF-UR5HZ1E6M-5342f93026b1-512")
+u2 = User.create(username:"swb258", location:"NYC", first_name:"Sean", profile_pic:"https://ca.slack-edge.com/T02MD9XTF-UR0NMQM42-034fb56f3580-512")
+u3 = User.create(username:"kgleeson", location:"Long Island", first_name:"Kevin", profile_pic:"https://ca.slack-edge.com/T02MD9XTF-URDG7620Y-1f912f45a62a-512")
+u4 = User.create(username:"srizzo", location:"Austin", first_name:"Summer", profile_pic:"https://ca.slack-edge.com/T02MD9XTF-UR22G3D7D-5b7b5152272d-512")
+u5 = User.create(username:"gmcguire", location:"Denver", first_name:"Gracie", profile_pic:"https://ca.slack-edge.com/T02MD9XTF-UD5PW68JW-55f48a258cbc-512")
+
 
 
 ## MOODS
@@ -22,9 +27,26 @@ m4 = Mood.create(name:"calm", words:"Calm,Peaceful,Pensive,Neutral,Chill", emoji
 
 
 ## QUOTES
-q1 = Quote.create(content: "as;figuhre;osa", author:"Sean")
+50.times do
+    Quote.create(content: Faker::Quote.yoda, author: Faker::Name.name)    
+end
 
 
 ## ENTRIES
-e1 = Entry.create(title:"Frist One", content:"Something", current_mood: "happy", image: "url", song: "url", user_id: u1.id, mood_id: m1.id, quote_id: q1.id)
-e2 = Entry.create(title:"Frist One", content:"Something", current_mood: "happy", image: "url", song: "url", user_id: u1.id, mood_id: m1.id)
+30.times do
+    assigned_mood = Mood.all.sample
+    assigned_mood_words = assigned_mood.words.split(",")
+    assigned_mood_emojis = assigned_mood.emojis.split(",")
+    combined_mood = assigned_mood_words + assigned_mood_emojis
+
+    Entry.create(title: Faker::Book.title, content: Faker::Lorem.paragraph(sentence_count: 10), current_mood: combined_mood.sample, image: Faker::Avatar.image, song: Faker::Avatar.image, user_id: u1.id, mood_id: assigned_mood.id, quote_id: rand(1..50))
+end
+
+100.times do
+    assigned_mood = Mood.all.sample
+    assigned_mood_words = assigned_mood.words.split(",")
+    assigned_mood_emojis = assigned_mood.emojis.split(",")
+    combined_mood = assigned_mood_words + assigned_mood_emojis
+
+    Entry.create(title: Faker::Book.title, content: Faker::Lorem.paragraph(sentence_count: 10), current_mood: combined_mood.sample, image: Faker::Avatar.image, song: Faker::Avatar.image, user_id: rand(2..5), mood_id: assigned_mood.id, quote_id: rand(1..50))
+end
